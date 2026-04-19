30d3b42c24c51fa768134eccb597c0a048d890c5
Anton Kashirov <anton.kashirov@gmail.com>
2026-04-19 22:30:25 +0700

    Revert "Add versioning engine boundary (#43)"
    This reverts commit 354f32f155971d7403715926ce21d643a268928c.

--- src/__generated__/client/browser.ts
+++ src/__generated__/client/browser.ts
@@ -38,11 +38,6 @@ export type Table = Prisma.TableModel
  */
 export type Row = Prisma.RowModel
 /**
- * Model ProjectVersioningConfig
- * 
- */
-export type ProjectVersioningConfig = Prisma.ProjectVersioningConfigModel
-/**
  * Model TableMigration
  * 
  */
--- src/features/versioning-engine/index.ts
+++
@@ -1,4 +0,0 @@
-export { VersioningEngineModule } from 'src/features/versioning-engine/versioning-engine.module';
-export { VersioningEngineService } from 'src/features/versioning-engine/services/versioning-engine.service';
-export { ProjectVersioningService } from 'src/features/versioning-engine/services/project-versioning.service';
-export type { VersioningMode } from 'src/features/versioning-engine/types/versioning-mode.types';
